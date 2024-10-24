b434840dffb7a9b8ff2d25904531ef04871dba0d
Mike Sperber <sperber@deinprogramm.de>
2024-10-24 09:36:19 +0200

    Update.

--- src/main/scala/Animals.scala
+++ src/main/scala/Animals.scala
@@ -62,7 +62,7 @@ object  Animals {
 
   extension (animals: List[Animal])
     def runOver = runOverAnimals(animals)
-      // animals.map(runOver)
+      animals.map(runOverAnimal)
 
   val highwayDead = highway.runOver
 
