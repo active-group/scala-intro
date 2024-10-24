aa1ea0a0e903a352c453dfebfa2e10109515d51e
Mike Sperber <sperber@deinprogramm.de>
2024-10-24 09:19:34 +0200

    Update.

--- src/main/scala/Animals.scala
+++ src/main/scala/Animals.scala
@@ -10,6 +10,15 @@ object  Animals {
   enum Animal {
     case Dillo(liveness: Liveness, weight: Weight)
     case Parrot(sentence: String, weight: Weight)
+
+    def runOver(): Animal =
+      this match {
+        case dillo@Dillo(liveness, weight) =>
+          // Dillo(Liveness.Dead, weight)
+          dillo.copy(liveness = Liveness.Dead)
+        case Parrot(sentence, weight) =>
+          Parrot("", weight)
+    }
   }
 
   val dillo1 = Animal.Dillo(Liveness.Alive, 10)
