bc8875a5ff1a611da7c36ed0a2e357ab887f19a0
Mike Sperber <sperber@deinprogramm.de>
2024-10-24 10:15:48 +0200

    Update.

--- src/main/scala/Animals.scala
+++ src/main/scala/Animals.scala
@@ -2,7 +2,7 @@ object  Animals {
 
   enum MyList[+A] { // Varianzannotation
     case Empty
-    case Cons(first: A, rest: List[A])
+    case Cons(first: A, rest: MyList[A])
   }
 
   type Weight = Double
